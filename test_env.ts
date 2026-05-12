3e5ab1d158998c599d3c41e5d9faf2771c667c0b
Toeic Factory Admin <admin@toeic-factory.local>
2026-05-12 12:23:56 +0700

    Clean up leftover scratch scripts and test files from root directory

--- test_env.ts
+++
@@ -1,1 +0,0 @@
-console.log(process.env.GEMINI_API_KEY ? 'HAS_KEY' : 'NO_KEY')
--- test_trans.ts
+++
@@ -1,2 +0,0 @@
-const url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=vi&dt=t&q=We+need+to+access+the+database";
-fetch(url).then(r => r.json()).then(d => console.log(d[0][0][0])).catch(console.error);
